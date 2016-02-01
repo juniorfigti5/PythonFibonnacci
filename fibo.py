42cb2d89d588c50fdb87c5702d46b90eea21076d
Wilson Guarin <wr.guarin234@uniandes.edu.co>
2016-02-01 11:18:44 -0500

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,21 @@
+# 1.Deberá construir una aplicación desarrollada en Python la cual calcula la serie de Fibonacci, mediante consola o de forma gráfica el usuario deberá ingresar el número de iteraciones desea calcular de la serie y mostrar la serie completa hasta dicho momento en forma lineal. Ej. N° de Iteraciones: 5 - Resultado esperado: “1, 2, 3, 5, 7”
+
+a=0
+b=1
+numero= input("Escriba el número de fibbonaci que quiere ") 
+lista=range(numero)
+i=0
+while i<numero:
+    if i == 0:
+        a=0
+        lista[i]=a
+    elif i==1:
+        b=1
+        lista[i]=b
+    else:
+        c=a+b
+        a=b
+        b=c
+        lista[i]=c
+    i+=1
+print lista
